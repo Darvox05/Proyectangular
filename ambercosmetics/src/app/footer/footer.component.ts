997b93f2c1584b42e40eee5aa324e9755dff4cc2
Darvox05 <139835322+Darvox05@users.noreply.github.com>
2024-01-05 23:08:23 -0500

    ajuste

--- ambercosmetics/src/app/footer/footer.component.ts
+++ ambercosmetics/src/app/footer/footer.component.ts
@@ -1,14 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule,RouterModule],
+  imports: [CommonModule,RouterModule, ReactiveFormsModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
 
+    formGroup: FormGroup = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email:  new FormControl('', [Validators.required]),
+      message:  new FormControl('', [Validators.required]),
+    });
+
+
+    sendForm() {
+      this.formGroup.markAllAsTouched();
+      if(this.formGroup.valid){
+        alert('Formulário enviado com sucesso!');
+        return;
+      }
+      alert('Formulário inválido!');
+      return;
+    }
 }
